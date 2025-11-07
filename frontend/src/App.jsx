0324b8e376f160dd3938841249fb042f35a6b689
SAMARTH GUPTA <samarthg.0812@gmail.com>
2025-11-08 02:56:05 +0530

    Update server URL to production endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ import { useEffect } from 'react'
 import { io } from 'socket.io-client'
 import { setSocket } from './redux/userSlice'
 
-export const serverUrl="http://localhost:8000"
+export const serverUrl="https://snapeats-backend-5i4a.onrender.com"
 function App() {
     const {userData}=useSelector(state=>state.user)
     const dispatch=useDispatch()
